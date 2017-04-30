c4fc8c8645a60fe96118ab9919736bd4820ffd2a
prashant <prshntchourasia@gmail.com>
2017-05-01 02:39:04 +0530

    crud operation on user

--- sb360/app/usermanagement/userController.js
+++ sb360/app/usermanagement/userController.js
@@ -1,7 +1,92 @@
 ﻿/// <reference path="C:\Users\prashant\documents\sb360\sb360\Scripts/angular.js" />
 
-myApp.controller("UserManagementController", function ($scope) {
-    console.log("user")
+myApp.controller("UserManagementController", function ($scope, UserApi) {
+    getuser();
+    function getuser() {
+        UserApi.getUsers()
+           .then(function (response) {
+               $scope.users = response.data;
+               console.log($scope.users);
+           })
+            .catch(function (response) {
+                console.error('Users error', response.status, response.data);
+            })
+            .finally(function () {
+                console.log("finally finished Users");
+            });
+    };
+        
+   
+        $scope.addUser = function () {
+            var UserToAdd = {
+                'FirstName': $scope.fname,
+                'LastName': $scope.lname,
+                'Email': $scope.email,
+                'UserRole': $scope.userRole
+            };
+            UserApi.AddUser(UserToAdd)
+            .then(function (response) {
+                alert("user added");
+                $scope.fname = undefined;
+                $scope.lname = undefined;
+                $scope.email = undefined;
+                $scope.userRole = undefined;
+                
+            })
+            .catch(function (response) {
+                console.error('Users error', response.status, response.data);
+            })
+            .finally(function () {
+                console.log("finally finished Users");
+            });
+        };
 
-    $scope.message = "I am is UserManagement View from another file"
+        $scope.editUser = function (user) {
+            console.log(user);
+            $scope.myModal = true;
+            $scope.fname = user.FirstName;
+            $scope.lname = user.LastName;
+            $scope.email = user.Email;
+            $scope.userRole = user.UserRole;
+            $scope.isEdit = true,
+            $scope.Id = user.Id
+        };
+        $scope.updateUser = function () {
+            var user = {
+                'Id': $scope.Id,
+                'FirstName': $scope.fname,
+                'LastName': $scope.lname,
+                'Email': $scope.email,
+                'UserRole': $scope.userRole
+            };
+            console.log(user)
+            UserApi.EditUser(user)
+             .then(function (response) {
+                 $scope.users = response.data;
+                 console.log($scope.users);
+             })
+            .catch(function (response) {
+                console.error('Users error', response.status, response.data);
+            })
+            .finally(function () {
+                console.log("finally finished Users");
+
+            });
+            getuser();
+        };
+
+        $scope.deleteUser = function (user) {
+            UserApi.DeleteUser(user)
+             .then(function (response) {
+                 $scope.users = response.data;
+                 console.log($scope.users);
+             })
+            .catch(function (response) {
+                console.error('Users error', response.status, response.data);
+            })
+            .finally(function () {
+                console.log("finally finished Users");
+            });
+            getuser();
+        };
 });
--- sb360/app/usermanagement/userService.js
+++ sb360/app/usermanagement/userService.js
@@ -1,1 +1,26 @@
-﻿
+﻿/// <reference path="C:\Users\prashant\documents\sb360\sb360\Scripts/angular.js" />
+
+myApp.factory("UserApi", function ($http) {
+    var baseUri = "http://sb360.azurewebsites.net/api";
+    var UserApi = {};
+
+    UserApi.getUsers = function () {
+        return $http.get(baseUri + "/users");
+
+
+    };
+    UserApi.AddUser = function (user) {
+        return $http.post(baseUri + '/users/', user);
+    }
+    UserApi.EditUser = function (user) {
+        return $http.put(baseUri + "/users/" + user.Id, user);
+    }
+    UserApi.DeleteUser = function (user) {
+        return $http.delete(baseUri + "/users/" + user.Id);
+    }
+    UserApi.getSpecificUser = function (user) {
+        var abc = $http.get(baseUri + "/users/" + user.Id);
+        return abc;
+    };
+    return UserApi;
+});
